8413e6adddf22186077e773322f822f40a7247de
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2022-04-27 11:44:40 -0400

    Created game theory file

--- game_theory.py
+++ game_theory.py
@@ -1,1 +1,14 @@
 # Lauren and Michelle
+import numpy
+
+# agent (1, 2, 3)
+# Starting:
+# Agent 1: selects an action uniformly at random
+# Agent 2: Start w/ globally highest payout
+# Agent 3: lowest payout
+
+
+# game (take two of the agents, and play them in the game)
+
+gameMatrix = numpy.matrix()
+print(gameMatrix)
